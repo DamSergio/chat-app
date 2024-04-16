2c9778ee6921f619386ac52e451b198daae36f27
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2024-04-16 16:24:12 +0200

    UI design

---
+++ frontend/src/components/sidebar/Conversations.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flax flex-col overflow-auto">
+      <Conversation />
+    </div>
+  );
+};
+
+export default Conversations;
